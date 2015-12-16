684a526a1ec20c88985f38a066c83db537ab64b1
Whitney Jenich <wjenich@gmail.com>
2015-12-16 13:25:16 -0600

    Checkpoint 5 Angular: Completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,13 +9,21 @@
          $stateProvider
              .state('landing', {
                  url: '/',
+                 controller: 'LandingCtrl as landing',
                  templateUrl: '/templates/landing.html'
             })
          
              .state('album', {
                  url: '/album',
                  templateUrl: '/templates/album.html'
+            })
+         
+            .state('collection', {
+                url: '/collection',
+                controller: 'CollectionCtrl as collection',
+                templateUrl: '/templates/collection.html'
             });
+           
      }
 
     angular
